65a15caecb7a3b5ae9e3db84b4eae4da768022c9
Alex <soyvitaly@gmail.com>
2021-12-22 10:54:35 +0100

    Split books by

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
 from livereload import Server
 import json
 
@@ -11,11 +12,14 @@ def on_reload():
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
-    rendered_page = template.render( books=parsed_books)
+    rendered_page = template.render(
+        parsed_books=list(chunked(parsed_books, 2))
+    )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
+on_reload()
 
 server = Server()
 server.watch('template.html', on_reload)
