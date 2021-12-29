4f1d820378cb60e9f1928641375455cca9898235
Alex <soyvitaly@gmail.com>
2021-12-29 10:07:22 +0100

    Dried the code

--- render_website.py
+++ render_website.py
@@ -8,23 +8,20 @@ import math
 
 def on_reload():
     with open('description.json', 'r', encoding="utf-8") as my_file:
-        parsed_books_json = my_file.read()
+        parsed_books = json.load(my_file)
 
-    parsed_books = json.loads(parsed_books_json)
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
     pages_folder = 'pages'
 
     amount_books_per_page = 20
-    pages_nambers = []
     pages = math.ceil(len(parsed_books) / amount_books_per_page)
-    for page in range(1, pages + 1):
-        pages_nambers.append(page)
+    pages_numbers = list(range(1, pages + 1))
     for number, books_per_page in enumerate(list(chunked(parsed_books, amount_books_per_page)), 1):
         rendered_page = template.render(
             books_per_page=list(chunked(books_per_page, 2)),
-            pages_nambers=pages_nambers,
+            pages_numbers=pages_numbers,
             current_page=number,
         )
         pathlib.Path(pages_folder).mkdir(parents=True, exist_ok=True)
